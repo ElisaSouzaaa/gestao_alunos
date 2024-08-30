f81230eb11ad67caf54990de6165079eb6ee59ce
Elisa Souza <contato.elisasouza@hotmail.com>
2024-08-30 17:44:10 -0300

    first commit

---
+++ src/main/java/com/api/genaws/GenAwsApplication.java
@@ -0,0 +1,13 @@
+package com.api.genaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenAwsApplication.class, args);
+    }
+
+}
